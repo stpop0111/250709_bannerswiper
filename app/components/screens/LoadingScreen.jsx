6fd29cdb5f7fd703536e268ff51e82c4888dba3d
stpop0111 <stpop0111@gmail.com>
2025-07-30 00:09:16 +0900

    新規作成: LoadingScreenコンポーネントを追加し、進行状況を表示するための基本的なレイアウトを実装

---
+++ app/components/screens/LoadingScreen.jsx
@@ -0,0 +1,9 @@
+export default function LoadingScreen({ progress, currentCount, totalCount }) {
+  return (
+    <div className='min-h-screen flex items-center justify-center p-2'>
+      <div className='w-full max-w-2xl mx-auto my-auto'>
+        
+      </div>
+    </div>
+  );
+}
