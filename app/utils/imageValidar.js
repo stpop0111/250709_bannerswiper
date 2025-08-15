d8a64b2b92b05390908ecd8b40959116d64b29c5
seita <stpop0111@gmail.com>
2025-08-15 11:19:40 +0900

    imageValidar.jsのリファクタリング

--- app/utils/imageValidar.js
+++ app/utils/imageValidar.js
@@ -1,33 +1,25 @@
 // 画像のURLが有効か確認
 // =======================================
-const checkImage = (url) => {
-  return new Promise((resolve) => {
-    // Promiseで非同期処理
-    const img = new Image();
-
-    img.onload = () => {
-      resolve({ url, isValid: true });
-    };
-
-    img.onerror = () => {
-      resolve({ url, isValid: false });
-    };
-
-    img.src = url;
-  });
-};
-
-// 画像のURLが有効か確認
-// =======================================
 export const validateImages = async (urlArray, onProgress) => {
-  const results = [];
+  // 内部関数として checkImage を定義
+  const checkImage = (url) => {
+    return new Promise((resolve) => {
+      const img = new Image();
 
+      img.onload = () => { resolve({ url, isValid: true })}; // ロードが成功したときの処理
+      img.onerror = () => { resolve({ url, isValid: false })}; // ロードが失敗したときの処理
+      img.src = url;
+    });
+  };
+  
+  // 
+  const results = [];
   for (let i = 0; i < urlArray.length; i++) {
     const result = await checkImage(urlArray[i]);
     results.push(result);
 
     if (onProgress) {
-      onProgress(i + 1, urlArray.length); // (current, total)として"InputScreen"に渡される
+      onProgress(i + 1, urlArray.length);
     }
   }
 
@@ -35,11 +27,13 @@ export const validateImages = async (urlArray, onProgress) => {
 };
 
 // 有効な画像URLの取得
+// =======================================
 export const getValidImages = (results) => {
   return results.filter((result) => result.isValid).map((result) => result.url);
 };
 
 // 無効な画像URLの取得
+// =======================================
 export const getInvalidImages = (results) => {
   return results
     .filter((result) => !result.isValid)
